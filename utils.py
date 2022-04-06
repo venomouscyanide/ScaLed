41dc698150710e410ee8a63c7603c52b8ad650a0
Shweta Jacob <shwetaaj59@gmail.com>
2022-04-06 14:38:14 -0400

    Update highest test accuracy to average test accuracy

--- utils.py
+++ utils.py
@@ -462,7 +462,8 @@ class Logger(object):
             print(f'Run {run + 1:02d}:', file=f)
             print(f'Highest Valid: {result[:, 0].max():.2f}', file=f)
             print(f'Highest Eval Point: {argmax + 1}', file=f)
-            print(f'   Final Test: {result[argmax, 1]:.2f}', file=f)
+            print(f'   Highest Test: {result[argmax, 1]:.2f}', file=f)
+            print(f'   Average Test: {result.T[1].mean():.2f}', file=f)
         else:
             result = 100 * torch.tensor(self.results)
 
@@ -478,7 +479,9 @@ class Logger(object):
             r = best_result[:, 0]
             print(f'Highest Valid: {r.mean():.2f} ± {r.std():.2f}', file=f)
             r = best_result[:, 1]
-            print(f'   Final Test: {r.mean():.2f} ± {r.std():.2f}', file=f)
+            r_revised = torch.reshape(result, (4, 2))[:, 1]
+            print(f'   Highest Test: {r.mean():.2f} ± {r.std():.2f}', file=f)
+            print(f'   Average Test: {r_revised.mean():.2f} ± {r_revised.std():.2f}', file=f)
 
 
 def draw_graph(graph):
