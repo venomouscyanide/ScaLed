Fix average test accuracy

utils.py
@@ -449,11 +449,16 @@ class Logger(object):
     def __init__(self, runs, info=None):
         self.info = info
         self.results = [[] for _ in range(runs)]
+        self.runs = runs
 
     def add_result(self, run, result):
         assert len(result) == 2
         assert run >= 0 and run < len(self.results)
         self.results[run].append(result)
+
+    def add_info(self, epochs, runs):
+        self.epochs = epochs
+        self.runs = runs
 
     def print_statistics(self, run=None, f=sys.stdout):
         if run is not None:
@@ -462,8 +467,8 @@ class Logger(object):
             print(f'Run {run + 1:02d}:', file=f)
             print(f'Highest Valid: {result[:, 0].max():.2f}', file=f)
             print(f'Highest Eval Point: {argmax + 1}', file=f)
-            print(f'   Highest Test: {result[argmax, 1]:.2f}', file=f)
-            print(f'   Average Test: {result.T[1].mean():.2f}', file=f)
+            print(f'Highest Test: {result[argmax, 1]:.2f}', file=f)
+            print(f'Average Test: {result.T[1].mean():.2f}', file=f)
         else:
             result = 100 * torch.tensor(self.results)
 
@@ -479,9 +484,9 @@ class Logger(object):
             r = best_result[:, 0]
             print(f'Highest Valid: {r.mean():.2f} ± {r.std():.2f}', file=f)
             r = best_result[:, 1]
-            r_revised = torch.reshape(result, (4, 2))[:, 1]
-            print(f'   Highest Test: {r.mean():.2f} ± {r.std():.2f}', file=f)
-            print(f'   Average Test: {r_revised.mean():.2f} ± {r_revised.std():.2f}', file=f)
+            r_revised = torch.reshape(result, (self.epochs * self.runs, 2))[:, 1]
+            print(f'Highest Test: {r.mean():.2f} ± {r.std():.2f}', file=f)
+            print(f'Average Test: {r_revised.mean():.2f} ± {r_revised.std():.2f}', file=f)
 
 
 def draw_graph(graph):