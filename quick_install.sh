a0d06bea3f65275388ae44de0dd38a48c09f6454
Paul Louis <iampaullouis@gmail.com>
2022-11-29 00:39:38 -0500

    QOL updates to quick install

--- quick_install.sh
+++ quick_install.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-conda create --name sweal_3_9 python=3.9 # don't use python 3.10 as it breaks the PyCharm debugger flow: https://youtrack.jetbrains.com/issue/PY-52137
+yes Y | conda create --name scaled_3_8 python=3.8 # don't use python 3.10 as it breaks the PyCharm debugger flow: https://youtrack.jetbrains.com/issue/PY-52137
+source /opt/homebrew/Caskroom/miniforge/base/bin/activate # point to your conda activate binary
 conda init bash
-conda activate sweal_3_9
-conda install pytorch torchvision -c pytorch # we use 1.11.0 version of pytorch
-pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.11.0+cpu.html # torch geometric version used is 2.0.4
-pip install matplotlib==3.5.2
-pip install ogb==1.3.3
-pip install networkx==2.8
+conda activate scaled_3_8
+yes Y | conda install pytorch torchvision -c pytorch
+export CC=/opt/homebrew/Cellar/llvm/13.0.1_1/bin/clang # point to the brew clang compiler
+pip install torch-sparse==0.6.13
+pip install torch-scatter torch-sparse  torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.11.0+cpu.html
+pip install matplotlib==3.6.1
+pip install ogb==1.3.5
+pip install networkx==2.8.7
 pip install pytorch_memlab==0.2.4
-pip install class_resolver==0.3.9
+pip install class_resolver==0.3.10
 pip install fast-pagerank==0.0.4
-pip install scikit-learn==1.1.0
+pip install scikit-learn==1.1.3
